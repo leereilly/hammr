2e5b086e0cbfbefeabf9a8b708acf2610ae3a174
Lee Reilly <lee@leereilly.net>
2011-07-04 13:12:05 -0700

    Added include Devise::TestHelpers to functional tests

--- test/functional/confirmations_controller_test.rb
+++ test/functional/confirmations_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ConfirmationsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
   # Replace this with your real tests.
   test "the truth" do
     assert true
--- test/functional/protected_content_controller_test.rb
+++ test/functional/protected_content_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProtectedContentControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
   test "should get index" do
     get :index
     assert_response :success
