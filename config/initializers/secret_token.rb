8c286172079750521d90b3bbae9faa99034b2ca1
Lee Reilly <lee@leereilly.net>
2011-07-03 19:31:03 -0700

    Skeleton rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hammr::Application.config.secret_token = 'ca6e6f63e85931a75449f23e925a2e0fe22c4cfee35d9653182e724a18a414f9be5f8c5c5f71d59dfb58d72e0ffe9b778c0dffeee28006c39bfe25a3a8041c81'
